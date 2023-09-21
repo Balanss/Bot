762f72336663ba5c03a4230f9f032a7ee765a546
Balans <gilmonpurry@hotmail.com>
2023-09-21 12:31:09 -0300

    rockoil bot

--- src/Comp/Main.jsx
+++ src/Comp/Main.jsx
@@ -1,81 +1,9 @@
-import React , {useEffect,useState} from 'react'
-import Functions from './Functions'
-import { auth, fs,db } from '../Firebase'
-import {collection,deleteDoc,doc,updateDoc} from "firebase/firestore";
-
-
+import React, { useEffect, useState } from "react";
 
 export default function Main() {
-const [name,setName] = useState([])
-const[middle,setMiddle] = useState([])
-
-
-useEffect(() => {
-  const script = document.createElement('script');
-  script.src = 'https://cdn.voiceflow.com/widget/bundle.mjs';
-  script.type = 'text/javascript';
-  script.async = true;
-  script.onload = () => {
-    window.voiceflow.chat.load({
-      verify: { projectID: import.meta.env.VITE_BOT },
-      url: 'https://general-runtime.voiceflow.com',
-      versionID: 'production',
-    });
-    // Introduce a small delay to ensure the widget is fully initialized
-    setTimeout(() => {
-      window.voiceflow.chat.open();
-    }, 3000); // Adjust the delay as needed
-
-  };
-  document.body.appendChild(script);
-
-  return () => {
-    // Cleanup if necessary
-    document.body.removeChild(script);
-  };
-}, []);
-
-
-
-
-
   return (
-    <div className='w-1/2'>
-
-<Functions name={name} setName={setName} setMiddle={setMiddle} />
-
-<section className='mt-10 mb-10 ml-5 text-white drop-shadow-xl ' >
-  <h1>  </h1>
-{name.map((x,id) => (
-<h1 className='text-6xl  drop-shadow-xl' key={id}> Hello,  {x.Name === 0 ? '':x.Name} </h1> 
-))}
-</section>
-
-<br/>
-<hr className='w-[50vw] border-yellow-500 border-b-4'/>
-
-<section className='mt-10 mb-10 ml-5 pb-10 text-white drop-shadow-xl'>
- {middle.map((x,id) => (
-
-<div key={id} className='text-2xl'>
-<li className=' drop-shadow-xl'>{x.Name} </li>
-
-<li className=' drop-shadow-xl'>{x.Location}</li>
-
-<li className=' drop-shadow-xl'>{x.description}</li>
-
-
-
-</div>
-
-
-))}   
-</section>
-
-
-
-
-
+    <div className="w-1/2">
+      <section className="mt-10 mb-10 ml-5 text-white drop-shadow-xl "></section>
     </div>
-  )
+  );
 }
