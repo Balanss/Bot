9cb44bce69fad900c1460d72b0907079d4953d8f
gilmon <gilmonpurry@hotmail.com>
2023-10-11 10:18:24 -0300

    live build

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,3 @@
-<<<<<<< HEAD
-import { useState , useEffect } from 'react'
-
-import './App.css'
-import Main from './Comp/Main'
-import Home from './Comp/Home/Home'
-import { HashRouter,Routes ,Route } from 'react-router-dom'
-import FoodBot from './Comp/FoodBot/FoodBot'
-
-
-function App() {
-
-
-
-
-  return (
-    <div >
-<HashRouter>
-<Routes>
-<Route path='/' element={<Home/>} />
-<Route path='/foodbot' element={<FoodBot/>} />
-<Route path='/main' element={<Main/>} />
-<Route path='/meds' element={<Main/>} />
-</Routes>
-</HashRouter>
-
-=======
 import "./App.css";
 import Main from "./Comp/Main";
 import Home from "./Comp/Home/Home";
@@ -42,7 +15,6 @@ function App() {
           <Route path="/meds" element={<Main />} />
         </Routes>
       </HashRouter>
->>>>>>> a5f11e48d7c7b3a6d08ccca5c8295f1901035008
     </div>
   );
 }
